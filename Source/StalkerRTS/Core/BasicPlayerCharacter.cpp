1c3f3bdb5cdfd4a4d452f37b60fc8dddf9600dd0
lammarx <perlamutrovy080@gmail.com>
2025-08-13 06:08:22 +0300

    Added child Blueprints from basic game C++ classes

---
+++ Source/StalkerRTS/Core/BasicPlayerCharacter.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StalkerRTS/Core/BasicPlayerCharacter.h"
+
+// Sets default values
+ABasicPlayerCharacter::ABasicPlayerCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABasicPlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABasicPlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABasicPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
