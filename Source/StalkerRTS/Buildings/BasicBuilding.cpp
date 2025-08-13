ad5e3e1c8b6ea262f992613e0f0143cd871b78ce
lammarx <perlamutrovy080@gmail.com>
2025-08-13 15:43:27 +0300

    Saved currrent project state

--- Source/StalkerRTS/Buildings/BasicBuilding.cpp
+++ Source/StalkerRTS/Buildings/BasicBuilding.cpp
@@ -2,13 +2,21 @@
 
 
 #include "StalkerRTS/Buildings/BasicBuilding.h"
+#include "Components/StaticMeshComponent.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Materials/MaterialInterface.h"
 
 // Sets default values
 ABasicBuilding::ABasicBuilding()
 {
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	RootComponent = Mesh;
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> MeshAsset(TEXT("/Game/Geometry/Meshes/1M_Cube.1M_Cube"));
+	if (MeshAsset.Succeeded())
+		Mesh->SetStaticMesh(MeshAsset.Object);
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
